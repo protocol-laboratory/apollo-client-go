ec2ca79dee8254f3c23c9bcc1f9e08791c3e2ec9
cy <710058301@qq.com>
2024-12-19 15:47:34 +0800

    feat: add logger context into inner client (#2)

--- apollo/client.go
+++ apollo/client.go
@@ -1,20 +1,35 @@
 package apollo
 
 import (
+	"context"
 	"crypto/tls"
 
 	"github.com/libgox/addr"
+	"golang.org/x/exp/slog"
 )
 
 type Client interface {
+}
+
+type innerClient struct {
+	ctx context.Context
 }
 
 type Config struct {
 	Address addr.Address
 	// TlsConfig configuration information for tls.
 	TlsConfig *tls.Config
+	// Logger structured logger for logging operations
+	Logger *slog.Logger
 }
 
 func NewClient(config *Config) (Client, error) {
-	return newClient(config)
+	if config.Logger != nil {
+		config.Logger = slog.Default()
+	}
+	ctx := context.Background()
+	c := &innerClient{
+		ctx: ctx,
+	}
+	return c, nil
 }
